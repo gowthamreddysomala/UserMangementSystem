e2b243c8e12848dcadf18f69733cc3150c8a1932
= <=>
2025-07-07 10:27:22 +0530

    V1 - Added The DB Schema

--- src/main/java/org/usermanag/UserManagementAPI/model/User.java
+++ src/main/java/org/usermanag/UserManagementAPI/model/User.java
@@ -4,15 +4,27 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
+    @NotBlank(message = "Password Should not be Blank")
+    private String Password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+    @CollectionTable(name = "user_roles" , joinColumns = @JoinColumn(name = "user_id"))
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,6 +35,7 @@ public class User {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
+    @Column(unique = true)
     private String email;
 
     // Timestamp fields
